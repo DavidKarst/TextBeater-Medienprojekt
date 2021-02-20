1055ef131655f60dee22241aa015f99a71b7e327
DavidKarst <73259498+DavidKarst@users.noreply.github.com>
2021-02-20 18:57:13 +0100

    Änderung

---
+++ TextBeater/Assets/Skript/Controll_Button.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controll_Button : MonoBehaviour
+{
+    private SpriteRenderer theSR;
+    public Sprite pressImage;
+    public Sprite defaultImage;
+
+    public KeyCode pressKey;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theSR = GetComponent<SpriteRenderer>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(pressKey))
+        {
+            theSR.sprite = pressImage;
+        }
+        if (Input.GetKeyUp(pressKey))
+        {
+            theSR.sprite = defaultImage;
+        }
+        
+    }
+}
