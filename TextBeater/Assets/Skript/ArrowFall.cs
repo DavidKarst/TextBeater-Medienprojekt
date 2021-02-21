15d239a363d72fbe3dda6f79efda6a6ab101be83
DavidKarst <73259498+DavidKarst@users.noreply.github.com>
2021-02-21 01:12:55 +0100

    Änderung

---
+++ TextBeater/Assets/Skript/ArrowFall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowFall : MonoBehaviour
+{
+    public float beatTempo;
+    public bool start;
+    // Start is called before the first frame update
+    void Start()
+    {
+        beatTempo = beatTempo / 60f;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!start)
+        {
+            if(Input.anyKeyDown)
+            {
+                start = true;
+            }
+        } else
+        {
+            transform.position -= new Vector3(0f, beatTempo * Time.deltaTime, 0f);
+        }
+        
+    }
+}
---
+++ TextBeater/Assets/Skript/NoteObjekt.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteObjekt : MonoBehaviour
+{
+    public bool pressd;
+    public KeyCode pressKey;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(pressKey))
+        {
+            if (pressd)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag=="Activator")
+        {
+
+            pressd = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Activator")
+        {
+
+            pressd = false;
+        }
+    }
+
+}
